ce53ac6bcf674174809df5c034b619b4c5532eee
gaurav4200 <114602714+gaurav4200@users.noreply.github.com>
2025-03-28 16:47:08 +0530

    Delete venting_renting directory

--- venting_renting/www/build/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { c as createRouter } from './index-74674300.js';
-import './index-0ae3ea8a.js';
-
-const Router = createRouter();
-
-export { Router };
-
-//# sourceMappingURL=index.esm.js.map
